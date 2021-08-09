Fixed issue that occurs when using a bender with greater than pi radians of bending

storageRingOptimization/HalbachLensClass.py
@@ -1,7 +1,6 @@
 import time
 import numpy as np
 import numpy.linalg as npl
-from interp3d import interp_3d
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -391,6 +390,7 @@ class HalbachLens:
 
         return rNew
     def _transform_Vector(self,v):
+        #todo: something seems wrong here with the matrix multiplixation
         #to evaluate the field from tilted or translated magnets, the evaluation point is instead tilted or translated,
         #then the vector is rotated back. This function handles the rotation of the evaluated vector
         #v: rows of vectors, shape (N,3) where N is the number of vectors
@@ -525,7 +525,6 @@ class SegmentedBenderHalbach(HalbachLens):
             #is clockwise about y axis in the xz plane looking from the negative side of y
             lens.position(r0)
             self.lensList.append(lens)
-
     def B_Vec(self,r):
         #r: coordinates to evaluate the field at. Either a (N,3) array, where N is the number of points, or a (3) array.
         #Returns a either a (N,3) or (3) array, whichever matches the shape of the r array
@@ -542,182 +541,3 @@ class SegmentedBenderHalbach(HalbachLens):
             return BArr[0]
         else:
             return BArr
-
-# lensFringe = SegmentedBenderHalbachLensFieldGenerator(.01, 1.0, self.ucAng, self.Lm,
-#                                                                           numLenses=3,inputOnly=True)
-
-
-# dxArr=np.logspace(-5,-15,num=30)
-# errorList=[]
-# for dx in dxArr:
-#     lens=HalbachLens(1,.0254,.05,length=.1)
-#     testArr=npl.norm(lens.BNorm_Gradient(coords,dr=dx,method=1),axis=1)
-#     error=1e2*np.sum(np.abs(testArr-sampleArr))/np.sum(sampleArr)
-#     errorList.append(error)
-#     print(dx,error)
-#
-# plt.title('Forward difference accuracy compared to central difference \n Central difference stepsize is 1e-6')
-# plt.xlabel('Step size ,m')
-# plt.ylabel("percent difference")
-# plt.loglog(dxArr,errorList,marker='o')
-# plt.grid()
-# plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import scipy.optimize as spo
-# numSpheresArr=np.asarray([1,2,3,4,5])
-# costList=[]
-# for numSpheres in numSpheresArr:
-#     print('---------',numSpheres)
-#     def cost(M):
-#         lens=HalbachLens(1,.0254,.05,4*.05,M=M[0],spherePerDim=numSpheres)
-#         BArr=lens.BNorm(particleCoords)
-#         error=1e2*np.sum(np.abs(BArr-vals))/np.sum(vals)
-#         # print(M[0],error)
-#         return error
-#     sol=spo.minimize(cost,np.asarray([1e6]))
-#     print(sol)
-#     costList.append(sol.fun)
-#
-# plt.title("Model accuracy compared to COMSOL versus sphere number")
-# plt.xlabel('Number of spheres along xy dimensions')
-# plt.ylabel('Percent error')
-# plt.semilogy(numSpheresArr,costList)
-# plt.grid()
-# plt.show()
-
-# lens=HalbachLens(1,.0254,.05,4*.05)
-# MArr=np.linspace(.95,1.05,num=30)*1e6
-# errorList=[]
-# for M in MArr:
-#     lens=HalbachLens(1,.0254,.05,4*.05,M=M)
-#     BArr=lens.BNorm(particleCoords)
-#     error=1e2*np.sum(np.abs(BArr-vals))/np.sum(vals)
-#     errorList.append(error)
-#     print(BArr.sum(),vals.sum())
-# plt.plot(MArr,errorList)
-# plt.show()
-
-
-#
-# L0=.5
-# lens=HalbachLens(1,.0254,.05,length=L0)
-# num=40
-# rMax=.04
-# posArr=np.linspace(-rMax,rMax,num=num)
-# coordsList=[]
-# for x in posArr:
-#     for y in posArr:
-#         if np.sqrt(x**2+y**2)<rMax:
-#             coordsList.append([x,y])
-# coords=np.asarray(coordsList)
-# planeCoords=np.column_stack((coords,np.zeros(coords.shape[0])))
-# zArr=np.linspace(3*.05,L0/2+5*.05,num=30)
-# resList=[]
-# sumList=[]
-# for z in zArr:
-#     planeCoords[:,2]=z
-#     BSum = npl.norm(lens.BNorm_Gradient(planeCoords), axis=1).sum()
-#     resList.append(BSum)
-#     print(z,sum(resList))
-#     sumList.append(sum(resList))
-# resArr=np.asarray(sumList)
-# resArr=np.abs(resArr-resArr[-1])
-# resArr=100*resArr/resArr[0]
-# plt.title('PercentField remainig of cumulative sum as a \n function of distance along magnet')
-# plt.semilogy(((zArr-L0/2)/.05)[:-1],resArr[:-1],marker='x')
-# plt.grid()
-# plt.xlabel('Distance from magnet edge, multiple of bore radius')
-# plt.ylabel('Percent of final value')
-# plt.show()
-
-
-
-
-# num=40
-# rMax=.045
-# posArr=np.linspace(-rMax,rMax,num=num)
-# coordsList=[]
-# posArrz=np.linspace(-.1,.1,num=num)
-# for x in posArr:
-#     for y in posArr:
-#         for z in posArrz:
-#             if np.sqrt(x**2+y**2)<rMax:
-#                 coordsList.append([x,y,z])
-# coords=np.asarray(coordsList)
-#
-# sphereNumArr=np.arange(2,12)
-# resList=[]
-# tList=[]
-# for sphereNum in sphereNumArr:
-#     print(sphereNum)
-#     t=time.time()
-#     lens=HalbachLens(1,.0254,.05,length=5*.0254,spherePerDim=sphereNum)
-#     BArr=lens.BNorm(coords)
-#     tList.append(time.time()-t)
-#     resList.append(np.sum(BArr))
-#
-# plt.title('Time to solve')
-# plt.xlabel('Number of spheres along xy dimensions')
-# plt.ylabel('Time,seconds')
-# plt.plot(sphereNumArr,tList,marker='x')
-# plt.show()
-#
-# resArr=np.asarray(resList)
-# resArr=100*np.abs(resArr-resArr[-1])/resArr[-1]
-# plt.title('Error from \'actual\' model. \n percent error from last value over sum of values from test points')
-# plt.grid()
-# plt.xlabel('Number of spheres along xy dimensions')
-# plt.ylabel('Percent error')
-# plt.semilogy(sphereNumArr[:-1],resArr[:-1],marker='x')
-# plt.show()
-#
-#
-# num=40
-# rMax=.04
-# posArr=np.linspace(-rMax,rMax,num=num)
-# coordsList=[]
-# for x in posArr:
-#     for y in posArr:
-#         if np.sqrt(x**2+y**2)<rMax:
-#             coordsList.append([x,y])
-# planeCoords=np.asarray(coordsList)
-# planeCoords=np.column_stack((planeCoords,np.zeros(planeCoords.shape[0])))
-#
-#
-# # FracArr = np.linspace(1, 10, num=25)
-# FracArr=np.arange(1,10.5,.5)
-# FracArr = np.append(FracArr, 30)
-# resList = []
-# rp=.05
-# for Frac in FracArr:
-#     print(Frac)
-#     lens = HalbachLens(1, .0254, rp, length=rp * Frac)
-#     BNorm = npl.norm(lens.BNorm_Gradient(planeCoords),axis=1)
-#     resList.append(np.sum(BNorm))
-# resArr = np.asarray(resList)
-# resArr = 100 * np.abs(resArr[-1] - resArr) / resArr[-1]
-# plt.title('Percent difference of total field values from \'actual\' \n value at z=0')
-# plt.xlabel('Magnet length as multiple of bore radius')
-# plt.ylabel('Percent difference')
-# plt.semilogy(FracArr[:-1], resArr[:-1],marker='x')
-# plt.grid()
-# plt.show()

storageRingOptimization/ParticleClass.py
@@ -203,7 +203,7 @@ class Particle:
                 qo=self.currentEl.transform_Lab_Coords_Into_Global_Orbit_Frame(self.q, self.cumulativeLength)
                 self.revolutions=qo[0]/totalLatticeLength
             self.currentEl=None # to save memory
-    def plot_Energies(self):
+    def plot_Energies(self,showOnlyTotalEnergy=False):
         if self.EArr.shape[0]==0:
             raise Exception('PARTICLE HAS NO LOGGED POSITION')
         EArr = self.EArr
@@ -213,8 +213,9 @@ class Particle:
         plt.close('all')
         plt.title('Particle energies vs position. \n Total initial energy is '+str(np.round(EArr[0],1)) +' energy units')
         plt.plot(qoArr[:,0],EArr-EArr[0],label='E')
-        plt.plot(qoArr[:, 0], TArr - TArr[0],label='T')
-        plt.plot(qoArr[:, 0], VArr - VArr[0],label='V')
+        if showOnlyTotalEnergy==False:
+            plt.plot(qoArr[:, 0], TArr - TArr[0],label='T')
+            plt.plot(qoArr[:, 0], VArr - VArr[0],label='V')
         plt.ylabel("Energy")
         plt.xlabel('Meters')
         plt.legend()

storageRingOptimization/SwarmTracerClass.py
@@ -53,6 +53,14 @@ class SwarmTracer:
             z[i]=(z[i-1]+alpha)%1
 
         return z
+    def initialize_Stablity_Testing_Swarm(self,qMax):
+        swarmTest = Swarm()
+        swarmTest.add_Particle(qi=np.asarray([-1e-10,0.0,0.0]))
+        swarmTest.add_Particle(qi=np.asarray([-1e-10, qMax, 0]))
+        swarmTest.add_Particle(qi=np.asarray([-1e-10, -qMax, 0]))
+        swarmTest.add_Particle(qi=np.asarray([-1e-10, 0, -qMax]))
+        swarmTest.add_Particle(qi=np.asarray([-1e-10, 0, qMax]))
+        return swarmTest
     def initialize_HyperCube_Swarm_In_Phase_Space(self, qMax, pMax, num, upperSymmetry=False):
         # create a cloud of particles in phase space at the origin. In the xy plane, the average velocity vector points
         # to the west. The transverse plane is the yz plane.
@@ -227,7 +235,6 @@ class SwarmTracer:
                     swarm.add_Particle(qi=qCoord.copy(),pi=pCoord.copy())
         if type(seed)==int:
             np.random.seed(int(time.time()))
-        # print('Number of particles generated: ', swarm.num_Particles())
         return swarm
     def move_Swarm_To_Combiner_Output(self,swarm,scoot=True,copySwarm=True):
         #take a swarm where at move it to the combiner's output. Swarm should be created such that it is centered at
@@ -271,6 +278,4 @@ class SwarmTracer:
         else:
             for i in range(swarmNew.num_Particles()):
                 swarmNew.particles[i]=self.particleTracer.trace(swarmNew.particles[i],h,T,fastMode=fastMode)
-
-
         return swarmNew

storageRingOptimization/elementPT.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pyximport;
 from math import sqrt
-pyximport.install(language_level=3, setup_args={'include_dirs': np.get_include()})
 import warnings
 import fastElementNUMBAFunctions
 from InterpFunction import generate_3DInterp_Function_NUMBA, generate_2DInterp_Function_NUMBA
@@ -334,7 +332,7 @@ class BenderIdeal(Element):
         # returns a 3d vector in the orbit frame. First component is distance along trajectory, second is radial displacemnt
         # from the nominal orbit computed with centrifugal force, and the third is the z axis displacemnt.
         qo = q.copy()
-        phi = self.ang - np.arctan2(q[1], q[0])  # angle swept out by particle in trajectory. This is zero
+        phi = self.ang - fast_Arctan2(qo)  # angle swept out by particle in trajectory. This is zero
         # when the particle first enters
         ds = self.ro * phi
         qos = ds
@@ -708,7 +706,7 @@ class BenderIdealSegmented(BenderIdeal):
 
     def transform_Element_Coords_Into_Local_Orbit_Frame(self, q):
         qo = q.copy()
-        phi = self.ang - np.arctan2(q[1], q[0])  # angle swept out by particle in trajectory. This is zero
+        phi = self.ang - fast_Arctan2(q) # angle swept out by particle in trajectory. This is zero
         # when the particle first enters
         ds = self.ro * phi
         qos = ds
@@ -779,13 +777,14 @@ class BenderIdealSegmentedWithCap(BenderIdealSegmented):
     def transform_Element_Coords_Into_Local_Orbit_Frame(self, q):
         # q: element coordinates (x,y,z)
         # returns qo: the coordinates in the orbit frame (s,xo,yo)
+
         qo = q.copy()
         angle = fast_Arctan2(qo)#np.arctan2(qo[1], qo[0])
         if angle < 0:  # restrict range to between 0 and 2pi
             angle += 2 * np.pi
 
         if angle < self.ang:  # if particle is in the bending angle section. Could still be outside though
-            phi = self.ang - np.arctan2(q[1], q[0])  # angle swept out by particle in trajectory. This is zero
+            phi = self.ang - angle  # angle swept out by particle in trajectory. This is zero
             # when the particle first enters
             qox = np.sqrt(q[0] ** 2 + q[1] ** 2) - self.ro
             qo[0] = self.ro * phi + self.Lcap  # include the distance traveled throught the end cap
@@ -804,7 +803,6 @@ class BenderIdealSegmentedWithCap(BenderIdealSegmented):
                     qo[0] = self.Lcap - qTest[1]
                     qo[1] = qTest[0] - self.ro
                 else:  # if in neither then it must be outside
-                    qo = np.empty(3)
                     qo[:] = np.nan
         return qo
 
@@ -1192,7 +1190,6 @@ class HalbachLensSim(LensIdeal):
         volumeCoords=np.asarray(np.meshgrid(xyArr,xyArr,zArr)).T.reshape(-1,3) #note that these coordinates can have
         #the wrong value for z if the magnet length is longer than the fringe field effects. This is intentional and
         #input coordinates will be shifted in a wrapper function
-
         BNormGrad = lens.BNorm_Gradient(volumeCoords)
         BNorm = lens.BNorm(volumeCoords)
         self.data3D = np.column_stack((volumeCoords, BNormGrad, BNorm))

storageRingOptimization/fastElementNUMBAFunctions.py
@@ -11,6 +11,8 @@ def transform_Unit_Cell_Force_Into_Element_Frame_NUMBA(Fx,Fy,Fz, q, M_uc, ucAng)
     # q: particle's position in the element frame where the force is acting
     phi = np.arctan2(q[1], q[0])  # the anglular displacement from output of bender to the particle. I use
     # output instead of input because the unit cell is conceptually located at the output so it's easier to visualize
+    if phi < 0:  # restrict range to between 0 and 2pi
+        phi += 2 * np.pi
     cellNum = int(phi // ucAng) + 1  # cell number that particle is in, starts at one
     if cellNum % 2 == 1:  # if odd number cell. Then the unit cell only needs to be rotated into that position
         rotAngle = 2 * (cellNum // 2) * ucAng
@@ -29,7 +31,10 @@ def transform_Unit_Cell_Force_Into_Element_Frame_NUMBA(Fx,Fy,Fz, q, M_uc, ucAng)
 @numba.njit()
 def transform_Element_Coords_Into_Unit_Cell_Frame_NUMBA(q, ang, ucAng):
     quc=np.empty(3)
-    phi = ang - np.arctan2(q[1], q[0])
+    angle=np.arctan2(q[1], q[0])
+    if angle < 0:  # restrict range to between 0 and 2pi
+        angle += 2 * np.pi
+    phi = ang - angle
     revs = int(phi // ucAng)  # number of revolutions through unit cell
     if revs % 2 == 0:  # if even
         theta = phi - ucAng * revs
@@ -50,7 +55,7 @@ def segmented_Bender_Sim_Force_NUMBA(q, ang, ucAng, numMagnets, rb, ap, M_ang,M_
     # force at point q in element frame
     # q: particle's position in element frame
     phi = np.arctan2(q[1], q[0])
-    if phi < 0:  # confine phi to be between 0 and 2pi
+    if phi < 0:  # restrict range to between 0 and 2pi
         phi += 2 * np.pi
     if phi <= ang:  # if particle is inside bending angle region
         rXYPlane=np.sqrt(q[0]**2+q[1]**2) #radius in xy plane