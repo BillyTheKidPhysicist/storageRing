Implemented hexapole combiner fully in storage ring. Fixed numerical accuracy issues with @ operator

storageRingOptimization/ParticleTracerClass.py
@@ -11,9 +11,10 @@ from shapely.geometry import Polygon,Point
 
 import elementPT
 
+#TODO: Why does the tracked time differe between fastMode=True and fastMode=False
 
 
-
+TINY_TIME_STEP=1e-9 #nanosecond time step to move particle from one element to another
 @numba.njit(numba.float64[:](numba.float64[:],numba.float64[:],numba.float64[:],numba.float64))
 def fast_qNew(q,F,p,h):
     return q+p*h+.5*F*h**2
@@ -23,37 +24,20 @@ def fast_pNew(p,F,F_n,h):
     return p+.5*(F+F_n)*h
 
 
-@numba.njit()
-def fast_2D_Mat_Mult(M,v):
-    vx0=v[0]
-    vy0=v[1]
-    vx=M[0,0]*vx0+M[0,1]*vy0
-    vy=M[1,0]*vx0+M[1,1]*vy0
-    v[0]=vx
-    v[1]=vy
 
-
-@numba.njit()
-def increment_Array_In_Place(q,dq):
-    q[0]=q[0]+dq[0]
-    q[1]=q[1]+dq[1]
-    q[2]=q[2]+dq[2]
-
-
-@numba.njit(numba.types.UniTuple(numba.float64[:],2)(numba.float64[:],numba.float64[:],numba.float64[:],numba.float64[:]
-                                                     ,numba.float64[:,:],numba.float64[:,:]))
-def transform_To_Next_Element(q,p,r01,r02,ROutEl1,RInEl2):
+# @numba.njit(numba.types.UniTuple(numba.float64[:],2)(numba.float64[:],numba.float64[:],numba.float64[:],numba.float64[:]
+#                                                      ,numba.float64[:,:],numba.float64[:,:]))
+def _transform_To_Next_Element(q,p,r01,r02,ROutEl1,RInEl2):
+    #don't try and condense. Because of rounding, results won't agree with other methods and tests will fail
     q=q.copy()
     p=p.copy()
-    fast_2D_Mat_Mult(ROutEl1,q)
-    increment_Array_In_Place(q,r01)
-    increment_Array_In_Place(q,-r02)
-    fast_2D_Mat_Mult(RInEl2,q)
-    fast_2D_Mat_Mult(ROutEl1,p)
-    fast_2D_Mat_Mult(RInEl2,p)
+    q[:2]=ROutEl1@q[:2]
+    q+=r01
+    q-=r02
+    q[:2]=RInEl2@q[:2]
+    p[:2]=ROutEl1@p[:2]
+    p[:2]=RInEl2@p[:2]
     return q,p
-
-
 #this class does the work of tracing the particles through the lattice with timestepping algorithms.
 #it utilizes fast numba functions that are compiled and saved at the moment that the lattice is passed. If the lattice
 #is changed, then the particle tracer needs to be updated.
@@ -63,7 +47,6 @@ class ParticleTracer:
         #lattice: ParticleTracerLattice object typically
         self.latticeElementList = lattice.elList  # list containing the elements in the lattice in order from first to last (order added)
         self.totalLatticeLength=lattice.totalLength
-
         self.numbaMultiStepCache=[]
         self.generate_Multi_Step_Cache()
 
@@ -109,7 +92,7 @@ class ParticleTracer:
             r02 = el2.r1
         ROutEl1 = el1.ROut
         RInEl2 = el2.RIn
-        return transform_To_Next_Element(q,p,r01,r02,ROutEl1,RInEl2)
+        return _transform_To_Next_Element(q,p,r01,r02,ROutEl1,RInEl2)
     def initialize(self):
         # prepare for a single particle to be traced
         self.T=0.0
@@ -193,7 +176,7 @@ class ParticleTracer:
         results=self.numbaMultiStepCache[self.currentEl.index](self.qEl,self.pEl,self.T,self.T0,self.h,self.currentEl.fieldFact)
         qEl_n,self.qEl,self.pEl,self.T,particleOutside=results
         if particleOutside is True:
-            self.check_If_Particle_Is_Outside_And_Handle_Edge_Event(qEl_n) #it doesn't quite make sense
+            self.check_If_Particle_Is_Outside_And_Handle_Edge_Event(qEl_n,self.pEl) #it doesn't quite make sense
             #that I'm doing it like this. The outside the element system could be revamped.
     def generate_Multi_Step_Cache(self):
         self.numbaMultiStepCache = []
@@ -308,30 +291,6 @@ class ParticleTracer:
             qf = qi + pi * dt  # the final position of the particle. Either at the end, or at some place where it
             # clipped, or where time ran out
             self.particle.log_Params_In_Drift_Region(qi, pi, qf, self.h,driftEl)
-    def handle_Element_Edge(self):
-        # This method uses the correct timestep to put the particle just on the other side of the end of the element
-        # using explicit euler.
-        el=self.currentEl
-        q=el.transform_Element_Coords_Into_Lab_Frame(self.qEl)
-        p=el.transform_Element_Frame_Vector_Into_Lab_Frame(self.pEl)
-
-        r=el.r2-q
-
-        rt=np.abs(np.sum(el.ne*r[:2])) #perindicular position  to element's end
-        pt=np.abs(np.sum(el.ne*p[:2]))#perpindicular momentum to surface of element's end
-        h=rt/pt
-        self.T += h-self.h #to account for the smaller step size here
-        q=q+p*h
-        eps=1e-9 #tiny step to put the particle on the other side
-        n_p=p/np.sum(np.sqrt(p**2)) #normalized vector of particle velocity direction
-
-        q=q+n_p*eps
-        #now the particle is in the next element!
-        return q,p
-
-
-
-
 
 
     def time_Step_Verlet(self):
@@ -347,12 +306,9 @@ class ParticleTracer:
             F=self.currentEl.force(qEl)
         #a = F # acceleration old or acceleration sub n
         qEl_n=fast_qNew(qEl,F,pEl,self.h)#q new or q sub n+1
-        # el= self.which_Element(qEl_n) # todo: a more efficient algorithm here will make up to a 17% difference. Perhaps
-        # #not always checking if the particle is inside the element by looking at how far away it is from and edge and
-        # #calculating when I should check again the soonest
         F_n=self.currentEl.force(qEl_n)
         if np.isnan(F_n[0]) == True: #particle is outside element if an array of length 1 with np.nan is returned
-            self.check_If_Particle_Is_Outside_And_Handle_Edge_Event(qEl_n)  #check if element has changed.
+            self.check_If_Particle_Is_Outside_And_Handle_Edge_Event(qEl_n,pEl)  #check if element has changed.
             return
         #a_n = F_n  # acceleration new or acceleration sub n+1
         pEl_n=fast_pNew(pEl,F,F_n,self.h)
@@ -361,17 +317,17 @@ class ParticleTracer:
         self.forceLast=F_n #record the force to be recycled
         self.elHasChanged = False# if the leapfrog is completed, then the element did not change during the leapfrog
 
-    def check_If_Particle_Is_Outside_And_Handle_Edge_Event(self,qEl_n):
+    def check_If_Particle_Is_Outside_And_Handle_Edge_Event(self,qEl,pEl):
         #this method checks if the element that the particle is in, or being evaluated, has changed. If it has
         #changed then that needs to be recorded and the particle carefully walked up to the edge of the element
         #This returns True if the particle has been walked to the next element with a special algorithm, or is when
         #using this algorithm the particle is now outside the lattice. It also return True if the provided element is
         #None. Most of the time this return false and the leapfrog algorithm continues
         #el: The element to check against
-        #todo: there are some issues here with element edges
+        qEl_Scooted=qEl+TINY_TIME_STEP*pEl #the particle might land right on an edge, scoot it along
         if self.accelerated==True:
             nextEl = self.get_Next_Element()
-            q_nextEl,p_nextEl=self.transform_To_Next_Element(qEl_n,self.pEl,nextEl)
+            q_nextEl,p_nextEl=self.transform_To_Next_Element(qEl_Scooted,pEl,nextEl)
             if nextEl.is_Coord_Inside(q_nextEl)==False:
                 self.particle.clipped=True
                 return
@@ -384,15 +340,15 @@ class ParticleTracer:
                 self.elHasChanged = True
                 return
         else:
-            el=self.which_Element(qEl_n) #
+            el=self.which_Element(qEl_Scooted)
             if el is None: #if outside the lattice
                 self.particle.clipped = True
                 return
             elif el is not self.currentEl: #element has changed
                 nextEl=el
                 self.particle.cumulativeLength += self.currentEl.Lo  # add the previous orbit length
-                qElLab=self.currentEl.transform_Element_Coords_Into_Lab_Frame(qEl_n) #use the old  element for transform
-                pElLab=self.currentEl.transform_Element_Frame_Vector_Into_Lab_Frame(self.pEl) #use the old  element for transform
+                qElLab=self.currentEl.transform_Element_Coords_Into_Lab_Frame(qEl_Scooted) #use the old  element for transform
+                pElLab=self.currentEl.transform_Element_Frame_Vector_Into_Lab_Frame(pEl) #use the old  element for transform
                 self.currentEl=nextEl
                 self.particle.currentEl=nextEl
                 self.qEl = self.currentEl.transform_Lab_Coords_Into_Element_Frame(qElLab)  # at the beginning of the next element
@@ -418,9 +374,10 @@ class ParticleTracer:
         nextEl=self.get_Next_Element()
         if nextEl.is_Coord_Inside(nextEl.transform_Lab_Coords_Into_Element_Frame(qElLab)) == True: #try the next element
             return nextEl
-        #now instead look everywhere, except the next element we already checked
-        for el in self.latticeElementList:
-            if el is not nextEl: #don't waste rechecking current element or next element
-                if el.is_Coord_Inside(el.transform_Lab_Coords_Into_Element_Frame(qElLab))==True:
-                    return el
-        return None
+        else:
+            #now instead look everywhere, except the next element we already checked
+            for el in self.latticeElementList:
+                if el is not nextEl: #don't waste rechecking current element or next element
+                    if el.is_Coord_Inside(el.transform_Lab_Coords_Into_Element_Frame(qElLab))==True:
+                        return el
+            return None

storageRingOptimization/ParticleTracerLatticeClass.py
@@ -82,9 +82,9 @@ class ParticleTracerLattice:
         PTL_Ring.add_Drift(.05)
         PTL_Ring.add_Halbach_Bender_Sim_Segmented_With_End_Cap(Lm,rp,numMagnetsHalfBend,rb,rOffsetFact=1.0)
         PTL_Ring.end_Lattice(enforceClosedLattice=False,constrain=False)
-        def errorFunc(rOffsetFact):
+        def errorFunc(outputOffsetFact):
             h=5e-6
-            PTL_Ring.elList[1].update_rOffset_Fact(rOffsetFact)
+            PTL_Ring.elList[1].update_rOffset_Fact(outputOffsetFact)
             PTL_Ring.build_Lattice()
             particle=Particle()
             particleTracer=ParticleTracer(PTL_Ring)
@@ -96,13 +96,13 @@ class ParticleTracerLattice:
                 error=np.std(1e6*particle.qoArr[:,1])
                 return error
             else: return np.nan
-        rOffsetFactArr=np.asarray([.8,.9,.933,.966,1.0,1.033,1.066,1.1,1.2])
+        outputOffsetFactArr=np.asarray([.8,.9,.933,.966,1.0,1.033,1.066,1.1,1.2])
         if parallel==True: njobs=-1
         else: njobs=1
-        errorArr=np.asarray(Parallel(n_jobs=njobs)(delayed(errorFunc)(rOffset) for rOffset in rOffsetFactArr))
-        rOptimal=self._find_rOptimal(rOffsetFactArr,errorArr)
-        return rOptimal
-    def _find_rOptimal(self,rOffsetFactArr,errorArr):
+        errorArr=np.asarray(Parallel(n_jobs=njobs)(delayed(errorFunc)(outputOffset) for outputOffset in outputOffsetFactArr))
+        rOffsetOptimal=self._find_rOptimal(outputOffsetFactArr,errorArr)
+        return rOffsetOptimal
+    def _find_rOptimal(self,outputOffsetFactArr,errorArr):
         test=errorArr.copy()[1:]
         test=np.append(test,errorArr[0])
         numValidSolutions=np.sum(~np.isnan(errorArr))
@@ -119,14 +119,14 @@ class ParticleTracerLattice:
         if valid==False:
             return None
         #trim out invalid points
-        rOffsetFactArr=rOffsetFactArr[~np.isnan(errorArr)]
+        outputOffsetFactArr=outputOffsetFactArr[~np.isnan(errorArr)]
         errorArr=errorArr[~np.isnan(errorArr)]
-        fit=spi.RBFInterpolator(rOffsetFactArr[:,np.newaxis],errorArr)
-        rOffsetFactArrDense=np.linspace(rOffsetFactArr[0],rOffsetFactArr[-1],10000)
-        newerrorArr=fit(rOffsetFactArrDense[:,np.newaxis])
-        rOptimal=rOffsetFactArrDense[np.argmin(newerrorArr)]
-        rMinDistFromEdge=np.min(rOffsetFactArr[1:]-rOffsetFactArr[:-1])/4
-        if rOptimal>rOffsetFactArr[-1]-rMinDistFromEdge or rOptimal<rOffsetFactArr[0]+rMinDistFromEdge:
+        fit=spi.RBFInterpolator(outputOffsetFactArr[:,np.newaxis],errorArr)
+        outputOffsetFactArrDense=np.linspace(outputOffsetFactArr[0],outputOffsetFactArr[-1],10000)
+        newerrorArr=fit(outputOffsetFactArrDense[:,np.newaxis])
+        rOptimal=outputOffsetFactArrDense[np.argmin(newerrorArr)]
+        rMinDistFromEdge=np.min(outputOffsetFactArr[1:]-outputOffsetFactArr[:-1])/4
+        if rOptimal>outputOffsetFactArr[-1]-rMinDistFromEdge or rOptimal<outputOffsetFactArr[0]+rMinDistFromEdge:
             # print('Invalid solution, rMin very near edge. ')
             return None
         return rOptimal
@@ -174,7 +174,9 @@ class ParticleTracerLattice:
         el=LensIdeal(self, L, Bp, rp, ap) #create a lens element object
         el.index = len(self.elList) #where the element is in the lattice
         self.elList.append(el) #add element to the list holding lattice elements in order
-        if constrain==True: self.set_Constrained_Linear_Element(el)
+        if constrain==True:
+            self.set_Constrained_Linear_Element(el)
+            print('not fully supported feature')
     def add_Drift(self,L,ap=.03):
         #Add element to the lattice. see elementPTPreFactor.py for more details on specific element
         #L: length of drift element, m
@@ -182,28 +184,28 @@ class ParticleTracerLattice:
         el=Drift(self,L,ap)#create a drift element object
         el.index = len(self.elList) #where the element is in the lattice
         self.elList.append(el) #add element to the list holding lattice elements in order
-    def add_Bender_Ideal_Segmented_With_Cap(self,numMagnets,Lm,Lcap,Bp,rp,rb,yokeWidth,space,rOffsetFact=1.0,ap=None):
+    def add_Bender_Ideal_Segmented_With_Cap(self,numMagnets,Lm,Lcap,Bp,rp,rb,yokeWidth,space,outputOffsetFact=1.0,ap=None):
         apFrac=.9 #apeture fraction
         if ap is None:#set the apeture as fraction of bore radius to account for tube thickness
             ap=apFrac*rp
         else:
             if ap > rp:
                 raise Exception('Apeture cant be bigger than bore radius')
-        el=BenderIdealSegmentedWithCap(self,numMagnets,Lm,Lcap,Bp,rp,rb,yokeWidth,space,rOffsetFact,ap)
+        el=BenderIdealSegmentedWithCap(self,numMagnets,Lm,Lcap,Bp,rp,rb,yokeWidth,space,outputOffsetFact,ap)
         el.index = len(self.elList)  # where the element is in the lattice
         self.benderIndices.append(el.index)
         self.elList.append(el)
     def add_Halbach_Bender_Sim_Segmented_With_End_Cap(self,Lm,rp,numMagnets,rb,extraSpace=0.0,rOffsetFact=1.0,apFrac=None):
         #Add element to the lattice. see elementPTPreFactor.py for more details on specific element
         #Lcap: Length of element on the end/input of bender
-        #rOffsetFact: factor to multply the theoretical offset by to minimize oscillations in the bending segment.
+        #outputOffsetFact: factor to multply the theoretical offset by to minimize oscillations in the bending segment.
         #modeling shows that ~.675 is ideal
         el = HalbachBenderSimSegmentedWithCap(self, Lm,rp,numMagnets,rb,extraSpace,rOffsetFact,apFrac,
                                               parallel=self.parallel)
         el.index = len(self.elList)  # where the element is in the lattice
         self.benderIndices.append(el.index)
         self.elList.append(el)
-    def add_Bender_Ideal_Segmented(self,numMagnets,Lm,Bp,rp,rb,yokeWidth,space,rOffsetFact,ap=None):
+    def add_Bender_Ideal_Segmented(self,numMagnets,Lm,Bp,rp,rb,yokeWidth,space,outputOffsetFact,ap=None):
         #Add element to the lattice. see elementPTPreFactor.py for more details on specific element
         #L: hard edge Length of individual magnet.
         #Bp: Field strength at pole face
@@ -221,7 +223,7 @@ class ParticleTracerLattice:
             if ap > rp:
                 raise Exception('Apeture cant be bigger than bore radius')
 
-        el=BenderIdealSegmented(self,numMagnets,Lm,Bp,rp,rb,yokeWidth,space,rOffsetFact,ap)
+        el=BenderIdealSegmented(self,numMagnets,Lm,Bp,rp,rb,yokeWidth,space,outputOffsetFact,ap)
         el.index = len(self.elList)  # where the element is in the lattice
         self.benderIndices.append(el.index)
         self.elList.append(el)
@@ -268,9 +270,9 @@ class ParticleTracerLattice:
     def end_Lattice(self,constrain=False,enforceClosedLattice=True,buildLattice=True,
                     surpressWarning=False):
         #TODO: THIS WHOLE THING IS WACK, especially the contraint part
-        #TODO: REALLY NEED TO CLEAN UP ERROR CATCHING
-        #TODO: document which parameters mean what when using constrain!
-        #TODO: THIS ALL NEEDS TO MAKE MORE SENSE
+        #REALLY NEED TO CLEAN UP ERROR CATCHING
+        #document which parameters mean what when using constrain!
+        #THIS ALL NEEDS TO MAKE MORE SENSE
         #call various functions to construct the lattice geometry, the shapely objects for visualization and finding particle
         # positions, catch errors, enforce constraints and fill parameter(s)
         # constrain: wether to constrain the lattice by varying parameters. typically to accomodate the presence of the
@@ -367,7 +369,8 @@ class ParticleTracerLattice:
         if self.bender1.segmented==True: #both bender1 and bender2 are same type
             params=self.solve_Implicit_Segmented_Triangle_Problem(inputAng,inputOffset, L1, L2)
         else:
-            params=self.solve_Triangle_Problem(inputAng, inputOffset, L1, L2, self.bender1.ro, self.bender2.ro)
+            params=self.solve_Triangle_Problem(inputAng, inputOffset,self.combiner.outputOffset, L1, L2,
+                                               self.bender1.ro, self.bender2.ro)
         # need to account for length of caps in bender
         params[-1]-=(self.bender1.Lcap+self.bender2.Lcap) #remove spurious length from straight section
         return params
@@ -394,13 +397,14 @@ class ParticleTracerLattice:
         D = rp + yokeWidth
         r10 = self.bender1.rb #nominal bending radius without including offset, ie center of bender
         r20 = self.bender2.rb #nominal bending radius without including offset, ie center of bender
+        outputOffset=self.combiner.outputOffset
         def cost(args,returnParams=False):
             r1,r2 = args
-            r1Offset=self.bender1.rOffsetFunc(r1) #must include offset in geometry calculation
-            r2Offset=self.bender2.rOffsetFunc(r2) #must include offset in geometry calculation
+            r1Offset=self.bender1.outputOffsetFunc(r1) #must include offset in geometry calculation
+            r2Offset=self.bender2.outputOffsetFunc(r2) #must include offset in geometry calculation
             r1+=r1Offset
             r2+=r2Offset
-            phi1,phi2,L3=self.solve_Triangle_Problem(inputAng,inputOffset,L1,L2,r1,r2)
+            phi1,phi2,L3=self.solve_Triangle_Problem(inputAng,inputOffset,outputOffset,L1,L2,r1,r2)
             r1-=r1Offset #remove offset for final step of finding number of magnets
             r2-=r2Offset #remove offset for final step of finding number of magnets
             UCAng1=np.arctan(.5*Lseg/(r1-D))
@@ -425,7 +429,7 @@ class ParticleTracerLattice:
 
     @staticmethod
     # @numba.njit(numba.float64[:](numba.float64,numba.float64,numba.float64,numba.float64,numba.float64,numba.float64))
-    def solve_Triangle_Problem(inputAng,inputOffset,L1,L2,r1,r2):
+    def solve_Triangle_Problem(inputAng,inputOffset,outputOffset,L1,L2,r1,r2):
         #the triangle problem refers to two circles and a kinked section connected by their tangets. This is the situation
         #with the combiner and the two benders. The geometry of the combiner requires an additional step because the orbit
         #does not go straight through the combiner input, but is shifted a small amount by an offset. This, combined with
@@ -439,9 +443,9 @@ class ParticleTracerLattice:
         #note that L1 and L2 INCLUDE the sections in the combiner.
         if inputAng is None:
             return np.pi,np.pi,L1
-        L1 += -inputOffset / np.tan(inputAng)
-        L2 +=- inputOffset * np.sin(inputAng) + inputOffset / np.sin(inputAng)
-
+        assert outputOffset>=0.0 #a positive value corresponds to a negative shift in y in element frame
+        L1 += -(inputOffset+outputOffset) / np.tan(inputAng)
+        L2 +=- inputOffset * np.sin(inputAng) + (inputOffset+outputOffset) / np.sin(inputAng)
 
         theta3 = np.pi - inputAng
         a = theta3 - np.arctan(r1 / L1) - np.arctan(r2 / L2)
@@ -545,6 +549,8 @@ class ParticleTracerLattice:
             if type(self.bender1)!=type(self.bender2):
                 raise Exception('BOTH BENDERS MUST BE THE SAME KIND')
         if constrain==True:
+            if self.latticeType!='storageRing':
+                raise Exception('Constrained lattice must be storage ring type')
             if type(self.bender1)!=type(self.bender2): #for continuous benders
                 raise Exception('BENDERS BOTH MUST BE SAME TYPE')
             if len(self.benderIndices) != 2:
@@ -610,15 +616,15 @@ class ParticleTracerLattice:
                     ye=yb+el.L*np.sin(el.theta)
                     el.nb = -np.asarray([np.cos(el.theta), np.sin(el.theta)])  # normal vector to input
                     el.ne = -el.nb #normal vector to end
-                    if prevEl.type=='BEND' or prevEl.type=='LENS_SIM_CAP':
-                        n=np.zeros(2)
-                        n[0]=-prevEl.ne[1]
-                        n[1]=prevEl.ne[0]
-                        dr=n*prevEl.rOffset
-                        xb+=dr[0]
-                        yb+=dr[1]
-                        xe+=dr[0]
-                        ye+=dr[1]
+                    #account for an offset if present
+                    n=np.zeros(2)
+                    n[0]=-prevEl.ne[1]
+                    n[1]=prevEl.ne[0]
+                    dr=n*prevEl.outputOffset
+                    xb+=dr[0]
+                    yb+=dr[1]
+                    xe+=dr[0]
+                    ye+=dr[1]
                     el.r0=np.asarray([(xb+xe)/2,(yb+ye)/2,0]) #center of lens or drift is midpoint of line connecting beginning and end
                 elif el.type=='BEND':
                     if prevEl.type in ('COMBINER_SQUARE','COMBINER_CIRCULAR'):
@@ -626,10 +632,10 @@ class ParticleTracerLattice:
                         # reference frame. This is based on the previous element
                     else:
                         el.theta=prevEl.theta-prevEl.ang
-                    if prevEl.type=="BENDER_SIM_SEGMENTED_CAP":
-                        rOffset=0
+                    if prevEl.type=="BENDER_SIM_SEGMENTED_CAP": #TODO: REMOVE?
+                        outputOffset=0
                     else:
-                        rOffset=el.rOffset
+                        outputOffset=el.outputOffset
                     #the bender can be tilted so this is tricky. This is a rotation about a point that is
                     #not the origin. First I need to find that point.
                     xc=xb+np.sin(el.theta)*el.rb #without including trajectory offset
@@ -641,10 +647,10 @@ class ParticleTracerLattice:
                     ye=np.sin(phi)*xb+np.cos(phi)*yb-xc*np.sin(phi)-yc*np.cos(phi)+yc
                     #accomodate for bending trajectory
 
-                    xb+=rOffset*np.sin(el.theta)
-                    yb+=rOffset*(-np.cos(el.theta))
-                    xe+=rOffset*np.sin(el.theta)
-                    ye+=rOffset*(-np.cos(el.theta))
+                    xb+=outputOffset*np.sin(el.theta)
+                    yb+=outputOffset*(-np.cos(el.theta))
+                    xe+=outputOffset*np.sin(el.theta)
+                    ye+=outputOffset*(-np.cos(el.theta))
 
 
 
@@ -668,7 +674,6 @@ class ParticleTracerLattice:
                     # and rotate it. Input is where nominal trajectory particle enters
                     drx = -(el.Lb + (el.La - el.inputOffset * np.sin(el.ang)) * np.cos(el.ang))
                     dry = -(el.inputOffset + (el.La - el.inputOffset * np.sin(el.ang)) * np.sin(el.ang))
-
 
 
                     el.r1El = np.asarray([0, 0])
@@ -705,7 +710,7 @@ class ParticleTracerLattice:
             if i==len(self.elList)-1: #if the last element then set the end of the element correctly
                 reo = el.r2.copy()
                 if el.type=='BEND':
-                    reo[1]-=el.rOffset
+                    reo[1]-=el.outputOffset
             i+=1
         #check that the last point matchs the first point within a small number.
         #need to account for offset.

storageRingOptimization/elementPT.py
@@ -90,9 +90,13 @@ class Element:
         self.bumpOffset=None # the transverse translational. Only applicable to bump lenses now
         self.bumpVector=np.zeros(3) #positoin vector of the bump displacement. zero vector for no bump amount
         self.sim = None  # wether the field values are from simulations
+        self.outputOffset = 0.0  # some elements have an output offset, like from bender's centrifugal force or
+        #lens combiner
         self.fieldFact = 1.0  # factor to modify field values everywhere in space by, including force
         self.fast_Numba_Force_Function=None #function that takes in only position and returns force. This is based on
         #arguments at the time of calling the function compile_Fast_Numba_Force_Function
+        self.maxCombinerAng=.1 #because the field value is a right rectangular prism, it must extend to past the
+        #end of the tilted combiner. This maximum value is used to set that extra extent, and can't be exceede by ang
     def compile_Fast_Numba_Force_Function(self):
         #function that generates the fast_Numba_Force_Function based on existing parameters. It compiles it as well
         #by passing dummy arguments
@@ -126,20 +130,13 @@ class Element:
     def transform_Lab_Frame_Vector_Into_Element_Frame(self, vec):
         # vec: 3D vector in lab frame to rotate into element frame
         vecNew = vec.copy()  # copying prevents modifying the original value
-        vecx = vecNew[0]
-        vecy = vecNew[1]
-        vecNew[0] = vecx * self.RIn[0, 0] + vecy * self.RIn[0, 1]
-        vecNew[1] = vecx * self.RIn[1, 0] + vecy * self.RIn[1, 1]
+        vecNew[:2]=self.RIn@vecNew[:2]
         return vecNew
-
     def transform_Element_Frame_Vector_Into_Lab_Frame(self, vec):
         # rotate vector out of element frame into lab frame
         # vec: vector in
         vecNew = vec.copy()  # copy input vector to not modify the original
-        vecx = vecNew[0];
-        vecy = vecNew[1]
-        vecNew[0] = vecx * self.ROut[0, 0] + vecy * self.ROut[0, 1]
-        vecNew[1] = vecx * self.ROut[1, 0] + vecy * self.ROut[1, 1]
+        vecNew[:2]=self.ROut@vecNew[:2]
         return vecNew
 
     def set_Length(self, L):
@@ -306,10 +303,8 @@ class BenderIdeal(Element):
         self.rb = rb  # bending radius of magnet. This is tricky because this is the bending radius down the center, but the
         # actual trajectory of the particles is offset a little out from this
         self.type = 'BEND'
-        self.rOffset = None  # the offset to the bending radius because of centrifugal force. There are two versions, one that
-        # accounts for reduced speed from energy conservation for actual fields, and one that doesn't
         self.ro = None  # bending radius of orbit, ie rb + rOffset.
-        self.rOffsetFunc = None  # a function that returns the value of the offset of the trajectory for a given bending radius
+        self.outputOffsetFunc = None  # a function that returns the value of the offset of the trajectory for a given bending radius
         self.segmented = False  # wether the element is made up of discrete segments, or is continuous
         self.capped = False  # wether the element has 'caps' on the inlet and outlet. This is used to model fringe fields
         self.Lcap = 0  # length of caps
@@ -319,9 +314,9 @@ class BenderIdeal(Element):
 
     def fill_Params(self):
         self.K = (2 * self.Bp * SIMULATION_MAGNETON / self.rp ** 2)  # 'spring' constant
-        self.rOffsetFunc = lambda rb: sqrt(rb ** 2 / 4 + self.PTL.v0Nominal ** 2 / self.K) - rb / 2
-        self.rOffset = self.rOffsetFunc(self.rb)
-        self.ro = self.rb + self.rOffset
+        self.outputOffsetFunc = lambda rb: sqrt(rb ** 2 / 4 + self.PTL.v0Nominal ** 2 / self.K) - rb / 2
+        self.outputOffset = self.outputOffsetFunc(self.rb)
+        self.ro = self.rb + self.outputOffset
         if self.ang is not None:  # calculation is being delayed until constraints are solved
 
             self.L = self.rb * self.ang
@@ -572,7 +567,7 @@ class CombinerSim(CombinerIdeal):
         fringeSpace = 5 * 1.1e-2
         apz = 6e-3
         super().__init__(PTL, Lm, np.nan, np.nan, np.nan,mode, sizeScale,
-                         fillsParams=False)  # TODO: replace all the Nones with np.nan
+                         fillsParams=False)
         self.sim = True
 
         self.space = fringeSpace * self.sizeScale  # extra space past the hard edge on either end to account for fringe fields
@@ -706,9 +701,9 @@ class BenderIdealSegmented(BenderIdeal):
 
     def fill_Params(self):
         super().fill_Params()
-        self.rOffsetFunc = lambda rb: self.rOffsetFact * (sqrt(
+        self.outputOffsetFunc = lambda rb: self.rOffsetFact * (sqrt(
             rb ** 2 / 4 + self.PTL.v0Nominal ** 2 / self.K) - rb / 2)
-        self.rOffset = self.rOffsetFunc(self.rb)
+        self.outputOffset = self.outputOffsetFunc(self.rb)
         self.Lseg = self.Lm + 2 * self.space
         if self.numMagnets is not None:
             self.ucAng = np.arctan((self.Lm / 2 + self.space) / (self.rb - self.yokeWidth - self.rp))
@@ -720,19 +715,14 @@ class BenderIdealSegmented(BenderIdeal):
 
     def transform_Lab_Coords_Into_Element_Frame(self, q):
         qNew = q - self.r0
-        qx = qNew[0]
-        qy = qNew[1]
-        qNew[0] = qx * self.RIn[0, 0] + qy * self.RIn[0, 1]
-        qNew[1] = qx * self.RIn[1, 0] + qy * self.RIn[1, 1]
+        qNew[:2]=self.RIn@qNew[:2]
         return qNew
 
     def transform_Lab_Frame_Vector_Into_Element_Frame(self, vec):
         # vec: 3D vector in lab frame to rotate into element frame
-        vecx = vec[0]
-        vecy = vec[1]
-        vec[0] = vecx * self.RIn[0, 0] + vecy * self.RIn[0, 1]
-        vec[1] = vecx * self.RIn[1, 0] + vecy * self.RIn[1, 1]
-        return vec
+        vecNew=vec.copy()
+        vecNew[:2]=self.RIn@vecNew[:2]
+        return vecNew
 
     def transform_Element_Coords_Into_Local_Orbit_Frame(self, q):
         qo = q.copy()
@@ -1002,7 +992,7 @@ class HalbachBenderSimSegmentedWithCap(BenderIdealSegmentedWithCap):
 
 
         self.K_Func=lambda r: a * r ** 2 + b * r + c
-        self.rOffsetFunc = lambda r:  self.rOffsetFact*(sqrt(
+        self.outputOffsetFunc = lambda r:  self.rOffsetFact*(sqrt(
             r ** 2 / 16 + self.PTL.v0Nominal ** 2 / (2 * self.K_Func(r))) - r / 4)  # this accounts for energy loss
     def fill_Params_Post_Constrained(self):
         self.ap=self.compute_Aperture()
@@ -1028,7 +1018,7 @@ class HalbachBenderSimSegmentedWithCap(BenderIdealSegmentedWithCap):
         m = np.tan(self.ang / 2)
         self.M_ang = np.asarray([[1 - m ** 2, 2 * m], [2 * m, m ** 2 - 1]]) * 1 / (1 + m ** 2)  # reflection matrix
         self.compile_Fast_Numba_Force_Function()
-        self.fill_rOffset_And_Dependent_Params(self.rOffsetFunc(self.rb))
+        self.fill_rOffset_And_Dependent_Params(self.outputOffsetFunc(self.rb))
     def make_Grid_Edge_Coord_Arr(self,Min,Max,stepSize=None, numSteps=None):
         assert Max>Min and Max-Min>TINY_STEP
         assert (not (stepSize is not None and numSteps is not None)) and (stepSize is not None or numSteps is not None)
@@ -1049,13 +1039,13 @@ class HalbachBenderSimSegmentedWithCap(BenderIdealSegmentedWithCap):
     def fill_rOffset_And_Dependent_Params(self,rOffset):
         #this needs a seperate function because it is called when finding the optimal rOffset rather than rebuilding
         #the entire element
-        self.rOffset=rOffset
-        self.ro=self.rb+self.rOffset
+        self.outputOffset=rOffset
+        self.ro=self.rb+self.outputOffset
         self.L=self.ang*self.rb
         self.Lo=self.ang*self.ro+2*self.Lcap
     def update_rOffset_Fact(self,rOffsetFact):
         self.rOffsetFact=rOffsetFact
-        self.fill_rOffset_And_Dependent_Params(self.rOffsetFunc(self.rb))
+        self.fill_rOffset_And_Dependent_Params(self.outputOffsetFunc(self.rb))
     def solve_Coords_Parallel(self,coords,lens):
         if self.parallel==False:
             BNormGradArr,BNormArr=lens.BNorm_Gradient(coords,returnNorm=True)
@@ -1480,7 +1470,7 @@ class CombinerHexapoleSim(Element):
             magnetWidthList.append(nextMagnetWidth)
         self.space=max(rpList)*outerFringeFrac
 
-        lens = _HalbachLensFieldGenerator(self.Lm, magnetWidthList, rpList, numSpherePerDim=4)
+        lens = _HalbachLensFieldGenerator(self.Lm, magnetWidthList, rpList, numSpherePerDim=2)
 
 
         numXY=int((self.ap/self.rp)*numPointsTransverse)
@@ -1490,9 +1480,9 @@ class CombinerHexapoleSim(Element):
         yArr_Quadrant=np.linspace(-TINY_STEP,1.1*self.rp+TINY_STEP,numXY)
         xArr_Quadrant=np.linspace(-(1.1*self.rp+TINY_STEP),TINY_STEP,numXY)
 
-
+        tiltedExtraSpace=2*self.maxCombinerAng*self.ap #extra space
         zMin=-TINY_STEP
-        zMax=self.Lm/2+self.space+TINY_STEP
+        zMax=self.Lm/2+self.space+tiltedExtraSpace
         zArr=np.linspace(zMin,zMax,num=numPointsLongitudinal) #add a little extra so interp works as expected
 
         volumeCoords=np.asarray(np.meshgrid(xArr_Quadrant,yArr_Quadrant,zArr)).T.reshape(-1,3) #note that these coordinates can have
@@ -1505,11 +1495,10 @@ class CombinerHexapoleSim(Element):
         # to the hard edge of the input in a straight line
 
 
-        idealOffset=self.find_Ideal_Offset()
-        inputAngle, inputOffset, qTracedArr, minSep=self.compute_Input_Angle_And_Offset(idealOffset)
-        print(inputAngle)
+        self.outputOffset=self.find_Ideal_Offset()
+        inputAngle, inputOffset, qTracedArr, minSep=self.compute_Input_Angle_And_Offset(self.outputOffset)
         # to find the length
-
+        assert np.abs(inputAngle)<self.maxCombinerAng #tilt can't be too large or it exceeds field region.
         self.Lo = self.compute_Trajectory_Length(
             qTracedArr)  # np.sum(np.sqrt(np.sum((qTracedArr[1:] - qTracedArr[:-1]) ** 2, axis=1)))
         self.L = self.Lo  # TODO: WHAT IS THIS DOING?? is it used anywhere
@@ -1695,6 +1684,8 @@ class CombinerHexapoleSim(Element):
             # through the combiner. Simple square apeture
             if np.sqrt(q[1]**2+q[2]**2) < self.ap:  # if inside the y (width) apeture
                 return True
+            else:
+                return False
         elif q[0] < 0:
             return False
         else:  # particle is in the bent section leading into combiner. It's bounded by 3 lines

storageRingOptimization/fastElementNUMBAFunctions.py
@@ -214,16 +214,16 @@ def combiner_Sim_Hexapole_Force_NUMBA(q, La, Lb, Lm, space, ang, ap, searchIsCoo
     FzSymmetryFact = 1.0 if z >= 0.0 else -1.0
     y = abs(y)  # confine to upper right quadrant
     z = abs(z)
-    fieldLength=Lm+2*space
-    if 0<=x <=fieldLength/2:
-        x = fieldLength/2 - x
+    symmetryLength=Lm+2*space
+    if 0<=x <=symmetryLength/2:
+        x = symmetryLength/2 - x
         Fx,Fy,Fz= force_Func(x, y, z)
         Fx=-Fx
-    elif fieldLength/2<x <= fieldLength:
-        x=x-fieldLength/2
+    elif symmetryLength/2<x:
+        x=x-symmetryLength/2
         Fx,Fy,Fz = force_Func(x, y, z)
     else:
-        return np.nan,np.nan,np.nan
+        raise Exception(ValueError)
     Fy = Fy * FySymmetryFact
     Fz = Fz * FzSymmetryFact
     return Fx,Fy,Fz

storageRingOptimization/full_Integration_Test.py
@@ -4,7 +4,7 @@ from ParticleTracerLatticeClass import ParticleTracerLattice
 from ParticleClass import Particle,Swarm
 import os
 from ParticleTracerClass import ParticleTracer
-
+#TODO: implement testing parallel versus serial
 testDataFolderPath=os.path.join(os.getcwd(),'testData')
 testSwarm=Swarm()
 testSwarm.add_Particle()
@@ -25,6 +25,7 @@ def save_TEST_Data(PTL,TESTDataFileName):
     np.savetxt(os.path.join(testDataFolderPath,TESTDataFilePath),np.asarray(testData))
     
 def TEST_Lattice_Tracing(PTL,fastMode,accelerated,TESTDataFileName):
+    np.set_printoptions(precision=100)
     TESTDataFilePath=os.path.join(testDataFolderPath,TESTDataFileName)
     swarmTracer=SwarmTracer(PTL)
     tracedSwarm=swarmTracer.trace_Swarm_Through_Lattice(testSwarm,1e-5,1.0,fastMode=fastMode,parallel=False